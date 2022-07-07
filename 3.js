36732a3d9d7d7742e65a056fb5d0c95a842313d0
Thiago9x <thimendonca44@gmail.com>
2022-07-06 23:59:11 -0300

    finalizado

--- 3.js
+++ 3.js
@@ -4,27 +4,147 @@
     Autor: Thiago Trentin
 ********************************************/
 
-let valorDias = [0, 1, 10, 30, 50, 20, 30, 30, 40].filter(
+let valorDias = [
+	{
+		"dia": 1,
+		"valor": 22174.1664
+	},
+	{
+		"dia": 2,
+		"valor": 24537.6698
+	},
+	{
+		"dia": 3,
+		"valor": 26139.6134
+	},
+	{
+		"dia": 4,
+		"valor": 0.0
+	},
+	{
+		"dia": 5,
+		"valor": 0.0
+	},
+	{
+		"dia": 6,
+		"valor": 26742.6612
+	},
+	{
+		"dia": 7,
+		"valor": 0.0
+	},
+	{
+		"dia": 8,
+		"valor": 42889.2258
+	},
+	{
+		"dia": 9,
+		"valor": 46251.174
+	},
+	{
+		"dia": 10,
+		"valor": 11191.4722
+	},
+	{
+		"dia": 11,
+		"valor": 0.0
+	},
+	{
+		"dia": 12,
+		"valor": 0.0
+	},
+	{
+		"dia": 13,
+		"valor": 3847.4823
+	},
+	{
+		"dia": 14,
+		"valor": 373.7838
+	},
+	{
+		"dia": 15,
+		"valor": 2659.7563
+	},
+	{
+		"dia": 16,
+		"valor": 48924.2448
+	},
+	{
+		"dia": 17,
+		"valor": 18419.2614
+	},
+	{
+		"dia": 18,
+		"valor": 0.0
+	},
+	{
+		"dia": 19,
+		"valor": 0.0
+	},
+	{
+		"dia": 20,
+		"valor": 35240.1826
+	},
+	{
+		"dia": 21,
+		"valor": 43829.1667
+	},
+	{
+		"dia": 22,
+		"valor": 18235.6852
+	},
+	{
+		"dia": 23,
+		"valor": 4355.0662
+	},
+	{
+		"dia": 24,
+		"valor": 13327.1025
+	},
+	{
+		"dia": 25,
+		"valor": 0.0
+	},
+	{
+		"dia": 26,
+		"valor": 0.0
+	},
+	{
+		"dia": 27,
+		"valor": 25681.8318
+	},
+	{
+		"dia": 28,
+		"valor": 1718.1221
+	},
+	{
+		"dia": 29,
+		"valor": 13220.495
+	},
+	{
+		"dia": 30,
+		"valor": 8414.61
+	}
+].filter(
     (faturamento) => {
-      return faturamento != 0;
+      return faturamento.valor != 0;
     }
   );
   let maiorValor = 0;
   let menorValor = Number.MAX_SAFE_INTEGER;
   let somaValorDias = 0;
 
-  for (var i = 0; i < valorDias.length; i++) {
+  for (let i = 0; i < valorDias.length; i++) {
     //maior valor
-    maiorValor = Math.max(maiorValor, valorDias[i]);
+    maiorValor = Math.max(maiorValor, valorDias[i].valor);
     //menor valor
-    menorValor = Math.min(menorValor, valorDias[i]);
+    menorValor = Math.min(menorValor, valorDias[i].valor);
     // quantidade de dias em que o valor de faturamento foi a cima da média
-    somaValorDias += valorDias[i];
+    somaValorDias += valorDias[i].valor;
   }
-
   let mediaValorDias = somaValorDias / valorDias.length;
   let valoresAcimaMedia = valorDias.filter((faturamento) => {
-    return faturamento > mediaValorDias;
+    return faturamento.valor > mediaValorDias;
   });
   console.log(maiorValor);
   console.log(menorValor);
